27cba246efaae561df1d858b00deab3464b76fbe
Shrey Gupta <shrey.gupta@instantsys.com>
2023-12-06 12:12:33 +0530

    initial commit

---
+++ GoodSpaceTask/LaunchScreen/LaunchScreen.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreen.swift
+//  GoodSpaceTask
+//
+//  Created by ShreyGupta on 06/12/23.
+//
+
+import Foundation
+import UIKit
+
+class LaunchScreen : UIViewController {
+    
+    //MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
